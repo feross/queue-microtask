49cf36f17acda35ba79f7d53648692baa29b1a12
Feross Aboukhadijeh <feross@feross.org>
2019-09-09 18:14:22 -0700

    delete outdated comment

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ module.exports = typeof queueMicrotask === 'function'
   ? queueMicrotask
   : typeof Promise === 'function'
     ? cb => Promise.resolve().then(cb)
-    : cb => setTimeout(cb, 0) // fallback for Node 10 and old browsers
+    : cb => setTimeout(cb, 0)
