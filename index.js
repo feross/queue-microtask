3e0473fc914cea72ac59718fdbc854aec75f95e2
Feross Aboukhadijeh <feross@feross.org>
2019-09-09 19:06:21 -0700

    Fix errors thrown by callback swallowed
    Fixes: https://github.com/feross/queue-microtask/issues/2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = typeof queueMicrotask === 'function'
   ? queueMicrotask
   : typeof Promise === 'function'
-    ? cb => Promise.resolve().then(cb)
+    ? cb => Promise.resolve()
+      .then(cb)
+      .catch(err => setTimeout(() => { throw err }, 0))
     : cb => setTimeout(cb, 0)
