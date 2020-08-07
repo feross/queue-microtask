76db08596845dab16d3058d23d1595e53f725a60
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-08-07 11:38:51 -0400

    chore: add index.mjs

---
+++ index.mjs
@@ -0,0 +1,9 @@
+/*! queue-microtask. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
+let promise
+
+export default typeof queueMicrotask === 'function'
+  ? queueMicrotask
+  // reuse resolved promise, and allocate it lazily
+  : cb => (promise || (promise = Promise.resolve()))
+    .then(cb)
+    .catch(err => setTimeout(() => { throw err }, 0))
