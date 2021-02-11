52366b7d4a98a31649ed24fecdcc04b6e16487e0
Mike Jacobson <michaeljjacobson1@gmail.com>
2021-02-10 18:03:21 -0800

    Prevent referencing globalThis when undefined
    Fixes #13

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 /*! queue-microtask. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 let promise
 
+function getGlobalObject() {
+  if (typeof globalThis === 'object') return globalThis;
+  if (typeof window !== 'undefined') return window;
+  return global;
+}
+
 module.exports = typeof queueMicrotask === 'function'
-  ? queueMicrotask.bind(globalThis)
+  ? queueMicrotask.bind(getGlobalObject())
   // reuse resolved promise, and allocate it lazily
   : cb => (promise || (promise = Promise.resolve()))
     .then(cb)
