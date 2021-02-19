819b2aefcc5b3c92ca6d2041f613d40e6ba0a863
yugasun <yuga_sun@163.com>
2021-02-19 11:27:32 +0800

    fix: add polyfill for globalThis

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /*! queue-microtask. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 let promise
 
+// globalThis polyfill
+(function (Object) {
+  if (typeof globalThis === 'object') return
+  Object.defineProperty(Object.prototype, '__magic__', {
+    get: function () {
+      return this
+    },
+    configurable: true // This makes it possible to `delete` the getter later.
+  })
+  // eslint-disable-next-line no-undef
+  __magic__.globalThis = __magic__
+  // eslint-disable-next-line no-undef
+  delete Object.prototype.__magic__
+}(Object))
+
 module.exports = typeof queueMicrotask === 'function'
   ? queueMicrotask.bind(globalThis)
   // reuse resolved promise, and allocate it lazily
